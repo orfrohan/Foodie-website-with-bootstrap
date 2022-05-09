304506b12ac1627583c78a24ae9317a555ae5dc7
orfrohan <soikotrohan08@gmail.com>
2022-05-09 13:33:17 +0600

    foodie webiste

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// active navbar
+
+let nav = document.querySelector(".navigation-warp");
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("scroll-on");
+  } else {
+    nav.classList.remove("scroll-on");
+  }
+};
+// nav hide
+
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+  a.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+  });
+});
+
+// counter design
+
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+    let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      stop = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+        if (current == end) {
+          clearInterval(timer);
+        }
+      }, stop);
+  }
+
+  counter("count1", 0, 1280, 3000);
+  counter("count2", 100, 1000, 3500);
+  counter("count3", 300, 1580, 2500);
+  counter("count4", 500, 2000, 4000);
+});
